7bb31ab7170452acb7839c2857faea11dbdc17b9
admin <root>
2021-03-24 10:44:22 +0300

    Add lesson1 homework

---
+++ src/ru/freeIt/homework/lesson1/Query1.java
@@ -0,0 +1,7 @@
+package ru.freeIt.homework.lesson1;
+//Набрать приведенный пример, откомпилировать его и запустить.
+public class Query1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/ru/freeIt/homework/lesson1/Query2.java
@@ -0,0 +1,24 @@
+package ru.freeIt.homework.lesson1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//Изменить данный пример так, что бы при запуске программы ваша программа
+//        выводила следующее: “Hello, <Ваше имя>!”.
+public class Query2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, Alexei!");
+        //helloMethod();
+    }
+//    public static void helloMethod(){
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Enter your name: ");
+//
+//        try {
+//            System.out.println("Hello, " + bufferedReader.readLine() + "!");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
